d77004ccadedca8cce7467b01219990a14060431
Hudson <hudson.sydney.software@gmail.com>
2024-12-14 23:51:56 +1100

    update

--- build/blockbanner/index.asset.php
+++ build/blockbanner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '302615610345dc667015');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '41e2015f402b9a3c904c');
