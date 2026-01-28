0b933df7887ee12747f9a519f8cb256a50d9dc23
Hudson <hudson.sydney.software@gmail.com>
2026-01-29 09:35:01 +1100

    css update

--- build/blockbanner/index.asset.php
+++ build/blockbanner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '16b7384d4481df5b884c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8d91ca85ed82f7b3aad9');
--- src/meetus/render.php
+++ src/meetus/render.php
@@ -6,33 +6,36 @@
 <div  <?php echo get_block_wrapper_attributes(); ?>>
   <section class="specialist-section">
         <h2>Meet Our Specialist Surgeon</h2>
+        <div class="profile-image-container">
+          <img src="<?php echo $attributes["imageUrl1"]?>" class="profile-image">
+        </div>
         <div class="surgeon-profile">
             <div class="profile-card">
-                <img src="<?php echo $attributes["imageUrl1"]?>" class="profile-image"> 
+                <!-- <img src="<?php echo $attributes["imageUrl1"]?>" class="profile-image">  -->
                   <div class="profile-content">
                   <div class="profile-details">
                      <h3 class="title"><?php echo $attributes["title1"]?></h3>
                      <p class="sub-title"><?php echo $attributes["wysiwygContent"]?></p>
-                    <p class="sub-p"><?php echo $attributes["wysiwygContent1"]?></p>
+                    <!-- <p class="sub-p"><?php echo $attributes["wysiwygContent1"]?></p> -->
                   </div>
                   <div class="btn-container">
                    <a href="<?php echo esc_url( $attributes['link1'] ); ?>">
-                      <button class="view-profile-btn">read more</button>
+                      <button class="view-profile-btn">learn more</button>
 </a>
                   </div>  
                 </div>
             </div>
             <div class="profile-card">
-                 <img src="<?php echo $attributes["imageUrl2"]?>" class="profile-image"> 
+                 <!-- <img src="<?php echo $attributes["imageUrl2"]?>" class="profile-image">  -->
                  <div class="profile-content">
                   <div class="profile-details">
                      <h3 class="title"><?php echo $attributes["title2"]?></h3>
                      <p class="sub-title"><?php echo $attributes["wysiwygContent2"]?></p>
-                    <p class="sub-p"><?php echo $attributes["wysiwygContent3"]?></p>
+                    <!-- <p class="sub-p"><?php echo $attributes["wysiwygContent3"]?></p> -->
                   </div>
                   <div class="btn-container">
                     <a href="<?php echo esc_url( $attributes['link2'] ); ?>">
-                      <button  class="view-profile-btn">read more</button>
+                      <button  class="view-profile-btn">learn more</button>
 </a>
                   </div>  
                 </div>
